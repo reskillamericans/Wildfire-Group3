e880ad3dd5fd0ff9b8184877db8762d5c9dcccf5
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-06-19 12:21:35 -0400

    Created models FireIncidient, RegisteredUser, EmergencyContact and question.

--- wildfire3/settings.py
+++ wildfire3/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wildfire3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wildfire-backend-python3',
+        'USER': 'pythontest',
+        'PASSWORD': 'pdai2@2o21SA',
+        'HOST': '68.183.121.201',
+        'PORT': '3306',
     }
 }
 
