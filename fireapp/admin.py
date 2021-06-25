38ead0736ae53f6d07a1a523cbcf7fd46709b15e
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-06-24 20:06:54 -0400

    Created CreateNewEmailSub model

--- fireapp/admin.py
+++ fireapp/admin.py
@@ -1,3 +1,9 @@
+from fireapp.models import EmergencyContact
 from django.contrib import admin
-
+from .models import RegisteredUser, EmergencyContact, Question, FireIncident, NewsletterEmailSub
 # Register your models here.
+admin.site.register(RegisteredUser)
+admin.site.register(EmergencyContact)
+admin.site.register(Question)
+admin.site.register(FireIncident)
+admin.site.register(NewsletterEmailSub)
--- fireapp/views.py
+++ fireapp/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from .models import NewsletterEmailSub
+from .forms import CreateNewEmailSubscription
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("<h1>Wildfire App 3</h1>")
+
+def newsletter(request):
+    
+
+    if request.method == 'POST':
+        form = CreateNewEmailSubscription(request.POST)
+
+        if form.is_valid():
+            new_email = form.cleaned_data["email"]
+            new_sub = CreateNewEmailSubscription(email=new_email)
+            new_sub.save()
+            
+       
+ 
+        
+    return render(request, "fireapp/newsletter.html", {"form": form})
+    
