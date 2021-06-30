1c247a32c8bc0b72392b9228a51a45739b3278fe
IsiHernandez <joishernandez87@gmail.com>
2021-06-29 22:27:00 -0500

    fixed toggle and hamburger menu

--- faq.js
+++ faq.js
@@ -1,13 +1,9 @@
-const buttons = document.querySelector('.images').querySelectorAll('a');
-const paragraphs = document.querySelector('.questions').querySelectorAll('p');
 
-const newDiv = document.createElement('div');
-newDiv.textContent = '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."'
-
-
-for( let i = 0; i < buttons.length; i++) {
-buttons[i].addEventListener('click', () => {
-   newDiv.classList.toggle('active');
-   paragraphs[i].append(newDiv);
-});
+const items = document.querySelectorAll(".accordion a");
+ 
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
 }
+ 
+items.forEach(item => item.addEventListener('click', toggleAccordion));
