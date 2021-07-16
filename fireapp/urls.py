92a609ac2b554c8c8e6dff4babce2d8c686dff45
Mila Wilson <monif64@gmail.com>
2021-07-16 21:40:04 +0200

    added app_name and path('search/...)

--- fireapp/urls.py
+++ fireapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+
+app_name= 'fireapp'
+
 urlpatterns = [
     path('', views.index, name="homepage"),
     path('about-us', views.about_us, name='about-us'),
     path('faq/', views.faq, name="faq"),
     path('contact/', views.contact, name="contact"),
+    path('search/', views.search, name='search'),
 ]
