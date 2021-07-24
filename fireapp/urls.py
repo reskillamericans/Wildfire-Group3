c709b7897d0bb63d8140b9c9247e56f2f391a56b
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-07-24 16:44:20 -0400

    Added update front-end html/css files to backend-test

--- fireapp/urls.py
+++ fireapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name="homepage"),
-    path('about-us', views.about_us, name='about-us'),
+    path('about-us/', views.about_us, name='about-us'),
     path('faq/', views.faq, name="faq"),
     path('contact/', views.contact, name="contact"),
 ]
