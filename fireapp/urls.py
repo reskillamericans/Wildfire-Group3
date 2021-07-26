b5d04893ab445652176e3b9174d4612ec0c87db0
Ennyola <medunoyeeni@gmail.com>
2021-07-27 00:32:43 +0100

    changed route for about us

--- fireapp/urls.py
+++ fireapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="homepage"),
-    path('about-us/', views.about_us, name='about-us'),
+    path('about/', views.about_us, name='about-us'),
     path('faq/', views.faq, name="faq"),
     path('contact/', views.contact, name="contact"),
 ]
